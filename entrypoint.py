82e04b1760211da8ee835de7d18df948ceb831b9
Amir Sezavar <asezavar@atlassian.com>
2023-06-07 12:07:32 +1000

    OSFJ-137 tidying up

--- entrypoint.py
+++ entrypoint.py
@@ -22,7 +22,7 @@ gen_cfg('dbconfig.xml.j2', f'{JIRA_HOME}/dbconfig.xml',
         user=RUN_USER, group=RUN_GROUP, overwrite=UPDATE_CFG)
 if env.get('atl_s3avatars_bucketname') and env.get('atl_s3avatars_region'):
     gen_cfg('filestore-config.xml.j2', f'{JIRA_HOME}/filestore-config.xml',
-        user=RUN_USER, group=RUN_GROUP, overwrite=UPDATE_CFG)
+        user=RUN_USER, group=RUN_GROUP, overwrite=True)
 if str2bool(env.get('clustered')):
     gen_cfg('cluster.properties.j2', f'{JIRA_HOME}/cluster.properties',
             user=RUN_USER, group=RUN_GROUP, overwrite=True)
